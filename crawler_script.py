c2aaf23b5db79e833536971a2c18ccfb38fa776e
Marc <petermarc@t-online.de>
2022-03-01 16:40:54 +0100

    Test commit

--- crawler_script.py
+++ crawler_script.py
@@ -9,7 +9,9 @@ import time
 urls = ["https://www.otto.de/p/nike-sportswear-air-force-1-le-gs-sneaker-1229702164/#variationId=1229702854", "https://www.otto.de/p/nike-sportswear-court-vision-low-sneaker-860786554/#variationId=860787754", "https://www.otto.de/p/ashley-brooke-by-heine-hosenanzug-1324918231/#variationId=1324918243"]
 
 i = 0
-price_tags = []
+prices = []
+titles = []
+
 
 # Öffnen mit Webbrowser
 # Test mit einfacher Schleife
@@ -24,18 +26,21 @@ while i < len(urls):
     #Preis extrahieren mit BeatifulSoup
     soup = BeautifulSoup(content,'lxml')
     price_tag = soup.find('div', class_ = "prd_price__main js_prd_price__main")
-    price_tags.append(price_tag)
+    prices.append(price_tag)
+
+    #title_tag = soup.find('div', class_ = "js_shortInfo__variationName prd_shortInfo__variationName").text
+    #titles.append(title_tag)
 
     i += 1
 
     #Preis in csv schreiben
 with open('shoe_prices.csv','w',newline='') as f:
         thewriter = writer(f)
-        header = ['Price']
+        header = ['Price', 'Title']
+        thewriter.writerow(header)
 
-        for price_tag in price_tags:
-            thewriter.writerow(header)
+        for price_tag in prices:
             price = price_tag.find('span', id = "normalPriceAmount").text
+            #title = titles
             info = [price]
             thewriter.writerow(info)
-

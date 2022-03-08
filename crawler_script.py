0a9d1844298403e90b7de236212c1ffb941017e1
Marc <petermarc@t-online.de>
2022-03-08 09:18:44 +0100

    Preise und Titel von mehreren URLs

--- crawler_script.py
+++ crawler_script.py
@@ -6,38 +6,41 @@ import time
 
 
 # Ziel Website
-urls = ["https://www.otto.de/p/nike-sportswear-air-force-1-le-gs-sneaker-1229702164/#variationId=1229702854", "https://www.otto.de/p/nike-sportswear-court-vision-low-sneaker-860786554/#variationId=860787754", "https://www.otto.de/p/ashley-brooke-by-heine-hosenanzug-1324918231/#variationId=1324918243"]
+#urls = ["https://www.otto.de/p/nike-sportswear-air-force-1-le-gs-sneaker-1229702164/#variationId=1229702854", "https://www.otto.de/p/nike-sportswear-court-vision-low-sneaker-860786554/#variationId=860787754", "https://www.otto.de/p/ashley-brooke-by-heine-hosenanzug-1324918231/#variationId=1324918243"]
 
 #Listen und Laufvariable definieren
 i = 0
 prices = []
 titles = []
 
+#URLs einlesen aus csv
+with open("import.csv") as file:
 
-# Öffnen mit Webbrowser
-# Test mit einfacher Schleife
-# kann man vlt lieber mit einer for schleife bauen?
-while i < len(urls):
-    time.sleep(1)
-    url = urls[i]
-    webbrowser.open(url,new = 2)
-    response = urllib.request.urlopen(url)
-    content = response.read()
+    # Öffnen mit Webbrowser
+    # Test mit einfacher Schleife
+    # kann man vlt lieber mit einer for schleife bauen?
 
-    #Preis extrahieren mit BeatifulSoup
-    soup = BeautifulSoup(content,'lxml')
+    for line in file:
+        time.sleep(1)
+        url = line
+        webbrowser.open(url,new = 2)
+        response = urllib.request.urlopen(url)
+        content = response.read()
 
-    #Preise in Lite speichern
-    price_tag = soup.find('div', class_ = "prd_price__main js_prd_price__main")
-    prices.append(price_tag)
+        #Preis extrahieren mit BeatifulSoup
+        soup = BeautifulSoup(content,'lxml')
 
-    #Titel in Liste speichern
-    title_tag = soup.find('div', class_ = "prd_module prd_module--noLine prd_shortInfo")
-    titles.append(title_tag)
+        #Preise in Lite speichern
+        price_tag = soup.find('div', class_ = "prd_price__main js_prd_price__main")
+        prices.append(price_tag)
 
-    i += 1
+        #Titel in Liste speichern
+        title_tag = soup.find('div', class_ = "prd_module prd_module--noLine prd_shortInfo")
+        titles.append(title_tag)
 
+        i += 1
 
+#csv öffnen
 #Preis in csv schreiben
 
 with open('shoe_prices.csv','w',encoding= 'utf-8',newline='') as f:

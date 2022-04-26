9454b5d1885523f62f2c94d6879a7d08cab5517e
Marc <petermarc@t-online.de>
2022-04-26 21:12:09 +0200

    playwright test

--- crawler_script.py
+++ crawler_script.py
@@ -7,6 +7,7 @@ from winreg import *
 import pandas as pd
 import os
 import pathlib
+from playwright.sync_api import sync_playwright
 
 
 def default_browser():
@@ -39,13 +40,13 @@ def path_def():
 
     p = find_path() + ' %s'
     path_tmp = pathlib.PureWindowsPath(p)
-    path = path_tmp.as_posix() 
+    path = path_tmp.as_posix()
     return path
 
 path = path_def()
 
-user_agent = open('user_agent.txt','r').readline().strip() #user agent aus Textdatei auf Variable zuweisen
-headers = {'User-Agent': user_agent}
+#user_agent = open('user_agent.txt','r').readline().strip() #user agent aus Textdatei auf Variable zuweisen
+#headers = {'User-Agent': user_agent}
 
 df = pd.DataFrame(columns=["url", "price", "inco_price", "name"])  # benoetigte dataframes initialisieren
 all_prices_df = pd.DataFrame(columns=["url", "price", "inco_price", "name"])
@@ -69,12 +70,21 @@ for row in df_input.itertuples():  # csv Input den Variablen zuweisen
 
     try:
 
-        webbrowser.get(path).open(url, new=1)  # browsertab oeffnen und Informationen abfragen
-        request = urllib.request.Request(url, None, headers)  # Anfrage mit header/ user agent
-        response = urllib.request.urlopen(request, timeout=5)   #timeout hinzugefügt
-        content = response.read()
+        #webbrowser.get(path).open(url, new=1)  # browsertab oeffnen und Informationen abfragen
+        #request = urllib.request.Request(url, None, headers)  # Anfrage mit header/ user agent
+        #response = urllib.request.urlopen(request, timeout=5)   #timeout hinzugefügt
+        #content = response.read()
+        with sync_playwright() as p:
+            browser = p.chromium.launch_persistent_context(
+                user_data_dir=r'C:\Users\Marc\AppData\Local\Google\Chrome\User Data', headless=False, slow_mo=1000,
+                user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36')
+            page = browser.new_page()
+            page.goto(url)
+            p_content = page.inner_html(pt_css_sel) #selektor für preis eingefügt
+            n_content = page.inner_html(nt_css_sel) #selektor für name eingefügt
 
-        soup = BeautifulSoup(content, 'lxml')
+
+        soup = BeautifulSoup(p_content, 'lxml')
 
         if pt_css_sel_attr == 'class':
             price_tag = soup.find(pt_css_sel, class_=pt_css_sel_name)
@@ -83,6 +93,8 @@ for row in df_input.itertuples():  # csv Input den Variablen zuweisen
         elif pt_css_sel_attr == 'id':
             price_tag = soup.find(pt_css_sel, id=pt_css_sel_name)
 
+        print(price_tag)
+
         if p_css_sel_attr == 'class':
             price = price_tag.find(p_css_sel, class_=p_css_sel_name).text.strip()
         elif p_css_sel_attr == 'id':
@@ -90,10 +102,15 @@ for row in df_input.itertuples():  # csv Input den Variablen zuweisen
         elif p_css_sel_attr == 'itemprop':
             price = price_tag.find(p_css_sel, itemprop=p_css_sel_name).text.strip()
 
+        soup = BeautifulSoup(n_content, 'lxml')
+
         if nt_css_sel_attr == 'class':
             name_tag = soup.find(nt_css_sel, class_=nt_css_sel_name)
         elif nt_css_sel_attr == 'id':
             name_tag = soup.find(nt_css_sel, id=nt_css_sel_name)
+
+        print(name_tag)
+        soup = BeautifulSoup(n_content, 'lxml')
 
         if n_css_sel_attr == 'class':
             name = name_tag.find(n_css_sel, class_=n_css_sel_name).text.strip()
@@ -104,17 +121,24 @@ for row in df_input.itertuples():  # csv Input den Variablen zuweisen
         elif n_css_sel_attr == 'individual':
             name_tmp = str(name_tag.find_all('h1'))
             name = soup.h1.text.strip()
-
+        time.sleep(0.5)
         close_tab()
         print(f"{shop} - successfully loaded")
 
-        path_inco = path + ' --incognito'  #informationen incognito abfragen
-        webbrowser.get(path_inco).open(url, new=2)
-        request = urllib.request.Request(url, None, headers)  # Anfrage mit header
-        response = urllib.request.urlopen(request, timeout=5)
-        content = response.read()
+        #path_inco = path + ' --incognito'  #informationen incognito abfragen
+        #webbrowser.get(path_inco).open(url, new=2)
+        #request = urllib.request.Request(url, None, headers)  # Anfrage mit header
+        #response = urllib.request.urlopen(request, timeout=5)
+        #content = response.read()
 
-        soup = BeautifulSoup(content, 'lxml')
+        with sync_playwright() as p:    #playwright hinzugefügt
+            browser = p.chromium.launch(headless=False, slow_mo=1000)
+            page = browser.new_page()
+            page.goto(url)
+            content_inco = page.inner_html(pt_css_sel) #Variable content inco erstell und selektor eingefügt
+
+
+        soup = BeautifulSoup(content_inco, 'lxml')
 
         if pt_css_sel_attr == 'class':
             inco_price_tag = soup.find(pt_css_sel, class_=pt_css_sel_name)
@@ -130,7 +154,7 @@ for row in df_input.itertuples():  # csv Input den Variablen zuweisen
 
         row = pd.DataFrame({'url': [url], 'price': [price], 'inco_price': [inco_price],'name': [name]})  # variablen in dataframe hinzufuegen
         df = pd.concat([df, row], ignore_index=True)
-        time.sleep(1)
+        time.sleep(0.5)
         close_tab()
         
     except AttributeError:
